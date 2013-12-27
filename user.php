9e3267848c364edc679fb25bb1b1012416d1be29
David Mallard <correspondence@davidmallard.id.au>
2013-12-27 20:32:24 +1100

    Add a user (author) archive template.

---
+++ user.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Ivory Tower's user template file.
+ *
+ * This is used to display user/author archive pages.
+ * 
+ * @since 0.1.0
+ * 
+ * @package Ivory_Tower
+ * @subpackage Templates
+ */
+
+get_header(); ?>
+
+<main <?php hybrid_attr( 'content' ); ?>>
+	<h2 <?php hybrid_attr( 'loop-title' ); ?>><?php hybrid_loop_title(); ?></h2>
+	<?php if ( have_posts() ) : // Initiate the loop by checking whether posts were found. ?>
+		<?php while ( have_posts() ) : // Loop through the posts. ?>
+			<?php the_post(); // Load the post data. ?>
+			<article <?php hybrid_attr( 'post' ); ?>>
+				<?php hybrid_get_content_template(); // Load the content/*.php template. ?>
+			</article>
+		<?php endwhile; // End looping through posts. ?>
+		<?php loop_pagination(); ?>
+	<?php else : // If no posts were found. ?>
+		<article <?php hybrid_attr( 'post' ); ?>>
+			<?php locate_template( array( 'content/error.php' ), true ); // Load the content/error.php template. ?>
+		</article>
+	<?php endif; // End the check for posts. ?>
+</main>
+
+<?php get_footer();
