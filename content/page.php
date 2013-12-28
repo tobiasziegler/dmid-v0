9e6309d9bf00961139c1dd9002b222a26687f839
David Mallard <correspondence@davidmallard.id.au>
2013-12-28 13:19:49 +1100

    Add a check for whether displaying a single page and ensure search results will only display the page excerpt.

--- content/page.php
+++ content/page.php
@@ -11,14 +11,26 @@
  * @package Ivory_Tower
  * @subpackage Templates
  */
-?>
 
-<header class="entry-header">
-	<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php the_title(); ?></h1>
-	<?php edit_post_link(); ?>
-</header>
+if ( is_page() ) : // Check if a single page is being displayed. ?>
 
-<div <?php hybrid_attr( 'entry-content' ); ?>>
-	<?php the_content(); ?>
-	<?php wp_link_pages(); ?>
-</div>
+	<header class="entry-header">
+		<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php the_title(); ?></h1>
+		<?php edit_post_link(); ?>
+	</header>
+	<div <?php hybrid_attr( 'entry-content' ); ?>>
+		<?php the_content(); ?>
+		<?php wp_link_pages(); ?>
+	</div>
+
+<?php else : // If not displaying a single page. ?>
+
+	<header class="entry-header">
+		<h1 <?php hybrid_attr( 'entry-title' ); ?>><a href="<?php the_permalink(); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
+		<?php edit_post_link(); ?>
+	</header>
+	<div <?php hybrid_attr( 'entry-summary' ); ?>>
+		<?php the_excerpt(); ?>
+	</div>
+
+<?php endif; // End the single page check. ?>
