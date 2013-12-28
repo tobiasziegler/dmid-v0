e40a04f93357eea3067975121801192737a68b04
David Mallard <correspondence@davidmallard.id.au>
2013-12-28 14:11:40 +1100

    Change the code structure for conditional checks.

--- content/page.php
+++ content/page.php
@@ -11,26 +11,28 @@
  * @package Ivory_Tower
  * @subpackage Templates
  */
+?>
 
-if ( is_page() ) : // Check if a single page is being displayed. ?>
-
-	<header class="entry-header">
+<header class="entry-header">
+	<?php if ( is_page() ) : // If viewing a single page, the title doesn't need a hyperlink. ?>		
 		<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php the_title(); ?></h1>
-		<?php edit_post_link(); ?>
-	</header>
-	<div <?php hybrid_attr( 'entry-content' ); ?>>
-		<?php the_content(); ?>
-		<?php wp_link_pages(); ?>
-	</div>
-
-<?php else : // If not displaying a single page. ?>
-
-	<header class="entry-header">
+	<?php else : // If not viewing a single page, include the hyperlink. ?>		
 		<h1 <?php hybrid_attr( 'entry-title' ); ?>><a href="<?php the_permalink(); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
-		<?php edit_post_link(); ?>
-	</header>
-	<div <?php hybrid_attr( 'entry-summary' ); ?>>
-		<?php the_excerpt(); ?>
-	</div>
+	<?php endif; // End the single page check. ?>
+	<?php edit_post_link(); ?>
+</header>
+
+<?php if ( is_page() ) : // If a single page is being displayed, show the full content. ?>
+
+<div <?php hybrid_attr( 'entry-content' ); ?>>
+	<?php the_content(); ?>
+	<?php wp_link_pages(); ?>
+</div>
+
+<?php else : // If not displaying a single page, display the excerpt. ?>
+
+<div <?php hybrid_attr( 'entry-summary' ); ?>>
+	<?php the_excerpt(); ?>
+</div>
 
 <?php endif; // End the single page check. ?>
