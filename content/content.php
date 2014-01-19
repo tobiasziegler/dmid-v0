344869143bfbf45ac6386d8f5d9a7d4d71458bff
David Mallard <correspondence@davidmallard.id.au>
2014-01-19 19:39:18 +1100

    Modify the base content template since it now is effectively the standard post template.

--- content/content.php
+++ content/content.php
@@ -4,7 +4,8 @@
  *
  * This is the base content template. It will be used every time the current
  * item in the loop doesn't have a more specific template available, e.g.,
- * based on post format or post type.
+ * based on post format or post type. For posts it should only be loaded for
+ * standard posts since there are templates for all other post formats.
  *
  * @since 0.1.0
  * 
@@ -26,25 +27,30 @@
 	<?php edit_post_link(); ?>
 </header>
 
-<?php if (
-		( !is_singular( get_post_type() ) ) // If this is not a singular post
-		|| ( has_excerpt() )                // or if the post has a manual excerpt
-		) :                                 // display the excerpt. ?>
+<?php if ( is_singular( get_post_type() ) ) : // If this is a singular post. ?>
+
+	<?php if ( has_excerpt() ) : // If the post has a manual excerpt. ?>
+
+	<div <?php hybrid_attr( 'entry-summary' ); ?>>
+		<?php the_excerpt(); ?>
+	</div>
+	
+	<?php endif; // End the manual excerpt check on a singular post. ?>
+
+	<div <?php hybrid_attr( 'entry-content' ); ?>>
+		<?php the_content(); ?>
+		<?php wp_link_pages(); ?>
+	</div>
+
+<?php else : // If this is not a singular post. ?>
+
+<?php get_the_image( array( 'size' => 'thumbnail', 'order' => array( 'featured', 'attachment' ) ) ); ?>
 
 <div <?php hybrid_attr( 'entry-summary' ); ?>>
 	<?php the_excerpt(); ?>
 </div>
 
-<?php endif; // End the check for whether to display the excerpt. ?>
-
-<?php if ( is_singular( get_post_type() ) ) : // If viewing a single post, display the content. ?>
-
-<div <?php hybrid_attr( 'entry-content' ); ?>>
-	<?php the_content(); ?>
-	<?php wp_link_pages(); ?>
-</div>
-
-<?php endif; // End the check for whether to display the full content. ?>
+<?php endif; // End the single post check. ?>
 
 <footer class="entry-meta">
 	<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'items_wrap' => '<ul %s><li>%s</li></ul>', 'sep' => '</li><li>' ) ); ?>
